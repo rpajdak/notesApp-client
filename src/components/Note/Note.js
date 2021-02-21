16834710245b3f0354eea2bed6470c977458c919
Rafal Pajdak <rpajdak@gmail.com>
2021-02-21 17:03:05 +0100

    Add function to delete note.

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -3,16 +3,29 @@ import {Link, NavLink} from "react-router-dom";
 import "../../css/list_of_notes.css"
 
 function Note(props) {
-    console.log(props)
-    console.log(props.note.content + "     tutaj")
-    console.log(props.note.title + "     tutaj")
+    function deleteNote(id) {
+
+        const noteRow = document.getElementById(props.note.id);
+        const request = fetch(`http://localhost:8080/notes/${props.note.id}`,
+            {
+                method: 'DELETE',
+                credentials: 'include',
+            }).then(Response => {
+            if (Response.status == 204) {
+                alert("Note has benn deleted");
+                noteRow.style.display = "none";
+            }
+        });
+
+    }
 
     return (
         <div className="list-of-notes">
-            <div className="single-note">
+            <div className="single-note" id={props.note.id}>
                 <p className="">Note title "{props.note.title}</p>
                 <p className="">Note content {props.note.content} </p>
                 <p className="">Note created {props.note.created} </p>
+                <button id="deleteBtn" onClick={deleteNote}>Delete this note</button>
             </div>
         </div>
     );
