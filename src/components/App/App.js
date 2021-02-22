ebc879bdbf7bda342728c5291b1f3414de5f5c74
Rafal Pajdak <rpajdak@gmail.com>
2021-02-22 12:02:24 +0100

    Add route to history and edit.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,10 @@
 import MainPaige from "../MainPaige/MainPaige"
 import NewNote from "../NewNote/NewNote";
 import AllNotes from "../AllNotes/AllNotes"
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-    Link,
-} from "react-router-dom";
-
+import AllDeletedNotes from "../AllDeletedNotes/AllDeletedNotes"
+import EditNote from "../EditNote/EditNote"
+import HistoryOfUpdatedNote from "../HistoryOfUpdatedNote/HistoryOfUpdatedNote";
+import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
 
 function App() {
     return (
@@ -16,7 +12,10 @@ function App() {
             <Switch>
                 <Route path="/" exact component={MainPaige}></Route>
                 <Route path="/add" exact component={NewNote}></Route>
-                <Route path="/all" exact component={AllNotes}></Route>}
+                <Route path="/all" exact component={AllNotes}></Route>
+                <Route path="/deleted" exact component={AllDeletedNotes}></Route>
+                <Route path="/history" exact component={HistoryOfUpdatedNote}></Route>
+                <Route path="/edit" exact component={EditNote}></Route>
             </Switch>
         </Router>
     );
